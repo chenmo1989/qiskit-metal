af04577cd36398730481d6f4ec26fe048a7d3e23
Mo <60524670+chenmo1989@users.noreply.github.com>
2025-02-22 14:17:28 -0800

    updates on clawcoupler

---
+++ qiskit_metal/qlibrary/user_components/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+User-defined Qiskit Metal components.
+"""
+
+# Import the ClawCoupler class to make it available in user_components
+from .claw_coupler import ClawCoupler
